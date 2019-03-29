47d0739c5ed65e9f46fb458b7f301a4e3e31ff49
何大洋 <he.jinyang@foxmail.com>
2019-03-29 16:31:15 +0800

    完成rest+ribbon和Feign两种负载均衡方式

---
+++ servicefeign/src/main/java/com/example/servicefeign/SchedualServiceHi.java
@@ -0,0 +1,14 @@
+package com.example.servicefeign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "service-hi")  //指定调用哪个服务
+@Service
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
---
+++ serviceribbon/src/main/java/com/example/serviceribbon/ServiceribbonApplication.java
@@ -0,0 +1,26 @@
+package com.example.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient //向服务中心注册 并且向程序的ioc注入一个bean: restTemplate
+public class ServiceribbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceribbonApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced    //表明这个restRemplate开启负载均衡的功能。
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
