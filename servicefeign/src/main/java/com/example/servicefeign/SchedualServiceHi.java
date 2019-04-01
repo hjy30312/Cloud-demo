15a72bd6aaf955161ee8819b444936c8f5943e92
何大洋 <he.jinyang@foxmail.com>
2019-04-01 09:12:44 +0800

    完成rest+ribbon和Feign两种断路器Hystrix的使用方式

--- servicefeign/src/main/java/com/example/servicefeign/SchedualServiceHi.java
+++ servicefeign/src/main/java/com/example/servicefeign/SchedualServiceHi.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-hi")  //指定调用哪个服务
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)  //指定调用哪个服务
 @Service
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
--- serviceribbon/src/main/java/com/example/serviceribbon/ServiceribbonApplication.java
+++ serviceribbon/src/main/java/com/example/serviceribbon/ServiceribbonApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient //向服务中心注册 并且向程序的ioc注入一个bean: restTemplate
+@EnableHystrix
 public class ServiceribbonApplication {
 
     public static void main(String[] args) {
